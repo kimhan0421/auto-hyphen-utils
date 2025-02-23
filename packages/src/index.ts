39e77a1793db03efe2f6ec9ce981df9902f1628f
Lily <kimhan0421@github.com>
2025-02-23 16:35:50 +0900

    feat: export autoHyphenPhoneNumber

--- packages/src/index.ts
+++ packages/src/index.ts
@@ -1,7 +1,9 @@
+import { autoHyphenPhoneNumber } from './autoHyphenPhoneNumber';
 import { commonAutoHyphen } from './common';
 
 const autoHyphen = {
   common: commonAutoHyphen,
+  phoneNumber: autoHyphenPhoneNumber,
 };
 
 export default autoHyphen;
