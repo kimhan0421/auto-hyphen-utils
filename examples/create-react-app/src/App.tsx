e4b3f94831afd73e5018f984eed69110a4edeeee
Lily <kimhan0421@github.com>
2025-02-23 15:27:14 +0900

    chore: example input

--- examples/create-react-app/src/App.tsx
+++ examples/create-react-app/src/App.tsx
@@ -8,7 +8,8 @@ function App() {
     <>
       <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="텍스트를 입력하세요" />
       {/* 입력된 텍스트에 autoHyphen 적용 결과 출력 */}
-      <p>Hyphen: {autoHyphen.common('01012345678', [3, 4, 4])}</p>
+      <p>Hyphen: {autoHyphen.common(text)}</p>
+      <p>Hyphen: {autoHyphen.phoneNumber(text)}</p>
     </>
   );
 }
