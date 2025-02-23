94e866c995114c2520c04e2c3af74119ba87cc74
Lily <kimhan0421@github.com>
2025-02-23 16:27:41 +0900

    fix:  resolve error encountered during test case

--- packages/src/common/commonAutoHyphen.ts
+++ packages/src/common/commonAutoHyphen.ts
@@ -1,32 +1,41 @@
-import isValidString from '../validators/isValidString';
+import { isValidString } from '../validators';
 
 /**
- * 지정된 패턴에 따라 입력 값에 하이픈을 자동으로 추가하는 함수
+ * @name 지정된 패턴에 따라 입력 값에 하이픈을 자동으로 추가하는 함수
  *
  * @param {string} value - 포맷할 입력 문자열입니다.
- * @param {Array<number>} pattern - 하이픈 삽입 위치를 지정하는 배열입니다.
- * @returns {string} - 하이픈이 추가된 문자열을 반환합니다.
+ * @param {Array<number>} pattern - 하이픈 삽입 위치를 지정하는 배열입니다. 기본값은 `[3, 4, 4]`
+ *  @returns {string} - 하이픈이 추가된 문자열을 반환합니다.
+ *   - 유효하지 않은 입력값이 주어진 경우 빈 문자열 (`""`)을 반환합니다.
+ *   - `pattern`이 유효하지 않은 경우, 숫자 문자열을 그대로 반환합니다.
  */
-export default function commonAutoHyphen(value: string, pattern: number[]): string {
-  if (!isValidString(value) || !Array.isArray(pattern)) {
-    throw new Error('유효하지 않은 입력 입니다.');
+export default function commonAutoHyphen(value?: string, pattern: number[] = []): string {
+  if (!value || !isValidString(value)) {
+    return '';
+  }
+
+  if (!Array.isArray(pattern) || pattern.length === 0 || pattern.some((p) => typeof p !== 'number' || p < 0)) {
+    return value;
   }
 
   const cleanValue = value.replace(/[^0-9]/g, '');
-  const result = pattern.reduce(
+
+  const { result } = pattern.reduce(
     (acc, length, index) => {
-      if (acc.position < cleanValue.length) {
-        const part = cleanValue.slice(acc.position, acc.position + length);
-        acc.result += part;
-        acc.position += length;
-        if (index < pattern.length - 1 && acc.position < cleanValue.length) {
-          acc.result += '-';
-        }
+      if (acc.position >= cleanValue.length) {
+        return acc;
       }
-      return acc;
+      if (index === pattern.length - 1) {
+        return { result: acc.result + cleanValue.slice(acc.position), position: cleanValue.length };
+      } else {
+        const part = cleanValue.slice(acc.position, acc.position + length);
+        const newPos = acc.position + length;
+        const hyphen = newPos < cleanValue.length ? '-' : '';
+        return { result: acc.result + part + hyphen, position: newPos };
+      }
     },
     { result: '', position: 0 }
   );
 
-  return result.result;
+  return result;
 }
