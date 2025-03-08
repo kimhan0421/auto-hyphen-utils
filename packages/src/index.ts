156a991c2433a1d289200e6fd8dff3863ecdc16a
Lily <kimhan0421@github.com>
2025-03-08 19:04:06 +0900

    feat:  add tinNumber support in autoHyphen

--- packages/src/index.ts
+++ packages/src/index.ts
@@ -1,9 +1,11 @@
 import { autoHyphenPhoneNumber } from './autoHyphenPhoneNumber';
+import { autoHyphenTinNumber } from './autoHyphenTinNumber';
 import { commonAutoHyphen } from './common';
 
 const autoHyphen = {
   common: commonAutoHyphen,
   phoneNumber: autoHyphenPhoneNumber,
+  tinNumber: autoHyphenTinNumber,
 };
 
 export default autoHyphen;
